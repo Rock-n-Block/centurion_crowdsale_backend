20920a1c44fb39f15b623f12a6a886897e86a3a5
yulian-khalitov <khalitov.yulian@yandex.ru>
2021-03-04 13:53:56 +0300

    upd: voucher email

--- centurion_crowdsale/vouchers/voucher_email.py
+++ centurion_crowdsale/vouchers/voucher_email.py
@@ -85,13 +85,18 @@ html_style='''<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "ht
     </style>
   </head>'''
 
-voucher_html_body = """<body ducatus bgcolor="#ffffff">
+voucher_html_body = """<body
+    ducatus=""
+    bgcolor="#ffffff"
+    style="margin: 0; padding: 0; min-width: 100%; font-family: sans-serif"
+  >
     <table
       width="100%"
       bgcolor="#ffffff"
       border="0"
       cellpadding="0"
       cellspacing="0"
+      style="border-collapse: separate"
     >
       <tr>
         <td>
@@ -102,9 +107,15 @@ voucher_html_body = """<body ducatus bgcolor="#ffffff">
             cellpadding="0"
             cellspacing="0"
             border="0"
+            style="border-collapse: separate; width: 100%; max-width: 1000px"
+            width="100%"
           >
             <tr>
-              <td style="border-radius: 0px" class="header">
+              <td
+                style="text-align: center; padding: 20px; border-radius: 0px"
+                class="header"
+                align="center"
+              >
                 <img
                   class="fix"
                   src="https://www.ducatuscoins.com/assets/img/centurion-logo.png"
@@ -112,14 +123,32 @@ voucher_html_body = """<body ducatus bgcolor="#ffffff">
                   height="133"
                   border="0"
                   alt=""
+                  style="height: auto"
                 />
               </td>
             </tr>
             <tr>
               <td style="padding-bottom: 50px">
-                <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                <table
+                  width="100%"
+                  border="0"
+                  cellspacing="0"
+                  cellpadding="0"
+                  style="border-collapse: separate"
+                >
                   <tr>
-                    <td class="accent">
+                    <td
+                      class="accent"
+                      style="
+                        padding: 10px 0 0px 0;
+                        font-size: 25px;
+                        font-weight: 400;
+                        color: #404040;
+                        text-transform: uppercase;
+                        text-align: center;
+                      "
+                      align="center"
+                    >
                       Congratulations, Your lease is now active!
                     </td>
                   </tr>
@@ -127,35 +156,93 @@ voucher_html_body = """<body ducatus bgcolor="#ffffff">
               </td>
             </tr>
             <tr>
-              <td style="padding-bottom: 50px" class="padding">
-                <table width="100%" border="0" cellspacing="0" cellpadding="0">
+              <td
+                style="
+                  padding-left: 30px;
+                  padding-right: 30px;
+                  padding-bottom: 50px;
+                "
+                class="padding"
+              >
+                <table
+                  width="100%"
+                  border="0"
+                  cellspacing="0"
+                  cellpadding="0"
+                  style="border-collapse: separate"
+                >
                   <tr>
-                    <td class="dear">Dear Partner,</td>
+                    <td
+                      class="dear"
+                      style="font-weight: 400; font-size: 20px; color: #404040"
+                    >
+                      Dear Partner,
+                    </td>
                   </tr>
                   <tr>
-                    <td class="text text-cont">
+                    <td
+                      class="text text-cont"
+                      style="
+                        font-size: 20px;
+                        color: #404040;
+                        font-weight: 300;
+                        padding-top: 20px;
+                      "
+                    >
                       Thank you for creating a Lease & Earn account on
                       centuriongm.com. We have received your payment and confirm
                       the following details:
                     </td>
                   </tr>
                   <tr>
-                    <td class="text text-cont">
+                    <td
+                      class="text text-cont"
+                      style="
+                        font-size: 20px;
+                        color: #404040;
+                        font-weight: 300;
+                        padding-top: 20px;
+                      "
+                    >
                       Tokens purchased: {tokens_purchased}
                     </td>
                   </tr>
                   <tr>
-                    <td class="text text-cont">
+                    <td
+                      class="text text-cont"
+                      style="
+                        font-size: 20px;
+                        color: #404040;
+                        font-weight: 300;
+                        padding-top: 20px;
+                      "
+                    >
                       Project Leased: {project_leased}
                     </td>
                   </tr>
                   <tr>
-                    <td class="text text-cont">
+                    <td
+                      class="text text-cont"
+                      style="
+                        font-size: 20px;
+                        color: #404040;
+                        font-weight: 300;
+                        padding-top: 20px;
+                      "
+                    >
                       Period of Lease: {period_of_lease} months
                     </td>
                   </tr>
                   <tr>
-                    <td class="text text-cont">
+                    <td
+                      class="text text-cont"
+                      style="
+                        font-size: 20px;
+                        color: #404040;
+                        font-weight: 300;
+                        padding-top: 20px;
+                      "
+                    >
                       Lease Repayment: First payment will be released to your
                       account on the 4th month of your lease, it would include
                       payments from month 1 to 3.
@@ -165,28 +252,115 @@ voucher_html_body = """<body ducatus bgcolor="#ffffff">
               </td>
             </tr>
             <tr>
-              <td style="padding-bottom: 50px" class="padding">
-                <table width="100%" border="0" cellspacing="0" cellpadding="0">
+              <td
+                style="
+                  padding-left: 30px;
+                  padding-right: 30px;
+                  padding-bottom: 50px;
+                "
+                class="padding"
+              >
+                <table
+                  width="100%"
+                  border="0"
+                  cellspacing="0"
+                  cellpadding="0"
+                  style="border-collapse: separate"
+                >
                   <tr>
-                    <td class="text text--center">
+                    <td
+                      class="text text--center"
+                      style="
+                        text-align: center;
+                        font-size: 20px;
+                        color: #404040;
+                        font-weight: 300;
+                      "
+                      align="center"
+                    >
                       Redeem your token into your Ducatus Wallet using this
                       Activation Code
                     </td>
                   </tr>
                   <tr>
-                    <td class="text code text--center">{activate_code}</td>
+                    <td
+                      class="text code text--center"
+                      style="
+                        text-align: center;
+                        font-size: 20px;
+                        color: #1737a0;
+                        font-weight: 300;
+                        padding-top: 5px;
+                      "
+                      align="center"
+                    >
+                      {activate_code}
+                    </td>
                   </tr>
                 </table>
               </td>
             </tr>
             <tr>
               <td
-                style="padding-bottom: 70px; padding-top: 10px"
+                style="
+                  padding-left: 30px;
+                  padding-right: 30px;
+                  padding-bottom: 50px;
+                "
                 class="padding"
               >
-                <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                <table
+                  width="100%"
+                  border="0"
+                  cellspacing="0"
+                  cellpadding="0"
+                  style="border-collapse: separate"
+                >
                   <tr>
-                    <td class="text">
+                    <td
+                      class="text"
+                      style="font-size: 20px; color: #404040; font-weight: 300"
+                    >
+                      <ul class="list" style="padding-left: 20px">
+                        <li style="margin-bottom: 10px">
+                          Open your Ducatus Web Wallet or Ducatus Wallet app
+                        </li>
+                        <li style="margin-bottom: 10px">
+                          On the menu, click Voucher then New Voucher
+                        </li>
+                        <li style="margin-bottom: 10px">
+                          Enter your Activation Code, then the address you want
+                          to send (Is it DUC Wallet or DUCX?)
+                        </li>
+                        <li style="margin-bottom: 10px">Click Activate</li>
+                      </ul>
+                    </td>
+                  </tr>
+                </table>
+              </td>
+            </tr>
+            <tr>
+              <td
+                style="
+                  padding-left: 30px;
+                  padding-right: 30px;
+                  padding-bottom: 70px;
+                  padding-top: 10px;
+                "
+                class="padding"
+              >
+                <table
+                  width="100%"
+                  border="0"
+                  cellspacing="0"
+                  cellpadding="0"
+                  style="border-collapse: separate"
+                >
+                  <tr>
+                    <td
+                      class="text"
+                      style="font-size: 20px; color: #404040; font-weight: 300"
+                    >
                       You are now on your way to building generational wealth.
                       Once again, thank you for supporting our vision for a
                       thriving cashless economy and for embarking on this
@@ -207,12 +381,31 @@ voucher_html_body = """<body ducatus bgcolor="#ffffff">
                   padding-right: 100px;
                 "
               >
-                <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                <table
+                  width="100%"
+                  border="0"
+                  cellspacing="0"
+                  cellpadding="0"
+                  style="border-collapse: separate"
+                >
                   <tr>
-                    <td class="text">Sincerely,</td>
+                    <td
+                      class="text"
+                      style="font-size: 20px; color: #404040; font-weight: 300"
+                    >
+                      Sincerely,
+                    </td>
                   </tr>
                   <tr>
-                    <td class="dear text-cont">
+                    <td
+                      class="dear text-cont"
+                      style="
+                        padding-top: 20px;
+                        font-weight: 400;
+                        font-size: 20px;
+                        color: #404040;
+                      "
+                    >
                       Ronny Tome, CEO The Emperor Group
                     </td>
                   </tr>
@@ -223,7 +416,6 @@ voucher_html_body = """<body ducatus bgcolor="#ffffff">
         </td>
       </tr>
     </table>
-
     <table
       bgcolor="#ffffff"
       class="content--footer"
@@ -231,15 +423,18 @@ voucher_html_body = """<body ducatus bgcolor="#ffffff">
       cellpadding="0"
       cellspacing="0"
       border="0"
+      style="border-collapse: separate; width: 100%"
+      width="100%"
     >
       <tr>
-        <td class="footer">
+        <td class="footer" style="text-align: right" align="right">
           <img
             class="fix"
             src="https://www.ducatuscoins.com/assets/img/centurion-img.jpg"
             width="80%"
             border="0"
             alt=""
+            style="height: auto"
           />
         </td>
       </tr>
